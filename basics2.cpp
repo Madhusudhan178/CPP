7d67a86597acad974fc71010c604fec31f6be355
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-01-11 21:33:57 +0530

    learning

--- basics2.cpp
+++ basics2.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int main()
 {
-     // Opertaion on data
-     // precedence and assocativity
-     cout<<boolalpha;//Now it will print only true or false rather than  0/1
+     
      
 
      return 0;
