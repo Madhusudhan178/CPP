27c47c44a176597920510716067d294de8103ccb
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-01-11 20:18:41 +0530

    learning

--- basics2.cpp
+++ basics2.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
-#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
      // Opertaion on data
      // precedence and assocativity
+     cout<<boolalpha;
+
      return 0;
 }
