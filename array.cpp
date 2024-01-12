d13fc0f7415a59417d231b87a1cede3acb4b2cc7
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-01-12 16:12:50 +0530

    commit

--- array.cpp
+++ array.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-     
+     int ar[] {0,20,50,60,90};
+     int arr[5];
+     for(int i=0;i<5;i++)
+     {
+          double a=arr[i];
+          cout<<&a<<" - ";
+          cout<<a<<endl;
+     }
 
 
      return 0;
